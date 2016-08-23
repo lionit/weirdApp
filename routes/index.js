dfd20c10955efcb61e9091deedc12979e8bb894b
lionit <lionit@gmail.com>
2016-08-23 19:28:14 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'); 
+var router = express.Router(); 
+/* GET home page. */ 
+router.get('/', function(req, res, next) { 
+  //res.render('index', { title: 'Express' }); 
+   res.writeHead(200); 
+   res.write("this is my website"); 
+     console.log("Test..."); 
+     res.end(); 
+ }); 
+ 
+ module.exports = router; 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
